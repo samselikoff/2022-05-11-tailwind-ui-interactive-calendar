51fc985f0f22516d30b326b90324d3439ddbdd2e
Sam Selikoff <sam.selikoff@gmail.com>
2022-05-11 14:00:38 -0400

    start of video

---
+++ pages/index-solution.jsx
@@ -0,0 +1,284 @@
+import { Menu, Transition } from '@headlessui/react'
+import { DotsVerticalIcon } from '@heroicons/react/outline'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid'
+import {
+  add,
+  eachDayOfInterval,
+  endOfMonth,
+  endOfWeek,
+  format,
+  getDay,
+  isEqual,
+  isSameDay,
+  isSameMonth,
+  parse,
+  parseISO,
+  startOfMonth,
+  startOfToday,
+  startOfWeek,
+} from 'date-fns'
+import { Fragment, useState } from 'react'
+
+const meetings = [
+  {
+    id: 1,
+    name: 'Leslie Alexander',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    startDatetime: '2022-05-11T13:00',
+    endDatetime: '2022-05-11T14:30',
+  },
+  {
+    id: 2,
+    name: 'Michael Foster',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    startDatetime: '2022-05-20T09:00',
+    endDatetime: '2022-05-20T11:30',
+  },
+  {
+    id: 3,
+    name: 'Dries Vincent',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    startDatetime: '2022-05-20T17:00',
+    endDatetime: '2022-05-20T18:30',
+  },
+]
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(' ')
+}
+
+export default function IndexPage() {
+  let today = startOfToday()
+  let [selectedDay, setSelectedDay] = useState(today)
+  let [currentMonth, setCurrentMonth] = useState(format(today, 'MMM-yyyy'))
+  let firstDayOfCurrentMonth = startOfMonth(
+    parse(currentMonth, 'MMM-yyyy', new Date())
+  )
+  let days = eachDayOfInterval({
+    start: startOfWeek(firstDayOfCurrentMonth),
+    end: endOfWeek(endOfMonth(firstDayOfCurrentMonth)),
+  }).map((date) => ({
+    date,
+  }))
+
+  let selectedDayMeetings = meetings.filter((meeting) =>
+    isSameDay(parseISO(meeting.startDatetime), selectedDay)
+  )
+
+  function next() {
+    let firstDayOfNextMonth = add(firstDayOfCurrentMonth, { months: 1 })
+    setCurrentMonth(format(firstDayOfNextMonth, 'MMM-yyyy'))
+  }
+
+  function previous() {
+    let firstDayOfNextMonth = add(firstDayOfCurrentMonth, { months: -1 })
+    setCurrentMonth(format(firstDayOfNextMonth, 'MMM-yyyy'))
+  }
+
+  return (
+    <div className="pt-16">
+      <div className="max-w-md px-4 mx-auto sm:px-7 md:max-w-4xl md:px-6">
+        <div className="md:grid md:grid-cols-2 md:divide-x md:divide-gray-200">
+          <div className="md:pr-14">
+            <div className="flex items-center">
+              <h2 className="flex-auto font-semibold text-gray-900">
+                {format(firstDayOfCurrentMonth, 'MMMM yyyy')}
+              </h2>
+
+              <button
+                onClick={next}
+                type="button"
+                className="-my-1.5 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+              >
+                <span className="sr-only">Previous month</span>
+                <ChevronLeftIcon className="w-5 h-5" aria-hidden="true" />
+              </button>
+              <button
+                onClick={previous}
+                type="button"
+                className="-my-1.5 -mr-1.5 ml-2 flex flex-none items-center justify-center p-1.5 text-gray-400 hover:text-gray-500"
+              >
+                <span className="sr-only">Next month</span>
+                <ChevronRightIcon className="w-5 h-5" aria-hidden="true" />
+              </button>
+            </div>
+            <div className="grid grid-cols-7 mt-10 text-xs leading-6 text-center text-gray-500">
+              <div>S</div>
+              <div>M</div>
+              <div>T</div>
+              <div>W</div>
+              <div>T</div>
+              <div>F</div>
+              <div>S</div>
+            </div>
+            <div className="grid grid-cols-7 mt-2 text-sm">
+              {days.map((day, dayIdx) => (
+                <div
+                  key={day.date}
+                  className={classNames(
+                    // dayIdx > 6 && 'border-t border-gray-200',
+                    dayIdx === 0 && colStartClasses[getDay(day.date)],
+                    'pb-2 pt-4'
+                  )}
+                >
+                  <button
+                    type="button"
+                    onClick={() => setSelectedDay(day.date)}
+                    className={classNames(
+                      isEqual(day.date, selectedDay) && 'text-white',
+                      !isEqual(day.date, selectedDay) &&
+                        isEqual(day.date, today) &&
+                        'text-red-500',
+                      !isEqual(day.date, firstDayOfCurrentMonth) &&
+                        !isEqual(day.date, today) &&
+                        isSameMonth(
+                          day.date,
+                          startOfMonth(firstDayOfCurrentMonth)
+                        ) &&
+                        'text-gray-900',
+                      !isEqual(day.date, firstDayOfCurrentMonth) &&
+                        !isEqual(day.date, today) &&
+                        !isSameMonth(
+                          day.date,
+                          startOfMonth(firstDayOfCurrentMonth)
+                        ) &&
+                        'text-gray-400',
+                      isEqual(day.date, selectedDay) &&
+                        isEqual(day.date, today) &&
+                        'bg-red-500',
+                      isEqual(day.date, selectedDay) &&
+                        !isEqual(day.date, today) &&
+                        'bg-gray-900',
+                      !isEqual(day.date, selectedDay) && 'hover:bg-gray-200',
+                      (isEqual(day.date, selectedDay) ||
+                        isEqual(day.date, today)) &&
+                        'font-semibold',
+                      'mx-auto flex h-8 w-8 items-center justify-center rounded-full'
+                    )}
+                  >
+                    <time dateTime={day.date}>{format(day.date, 'd')}</time>
+                  </button>
+                  <div className="w-1 h-1 mx-auto mt-1">
+                    {meetings.some((meeting) =>
+                      isSameDay(parseISO(meeting.startDatetime), day.date)
+                    ) && (
+                      <div className="w-1 h-1 rounded-full bg-sky-500"></div>
+                    )}
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+          <section className="mt-12 md:mt-0 md:pl-14">
+            <h2 className="font-semibold text-gray-900">
+              Schedule for{' '}
+              <time dateTime={format(selectedDay, 'yyyy-MM-dd')}>
+                {format(selectedDay, 'MMM d, yyyy')}
+              </time>
+            </h2>
+            <ol className="mt-4 space-y-1 text-sm leading-6 text-gray-500">
+              {selectedDayMeetings.length > 0 ? (
+                selectedDayMeetings.map((meeting) => (
+                  <Meeting meeting={meeting} key={meeting.id} />
+                ))
+              ) : (
+                <p>No meetings today.</p>
+              )}
+            </ol>
+          </section>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Meeting({ meeting }) {
+  let startDatetime = parseISO(meeting.startDatetime)
+  let endDatetime = parseISO(meeting.endDatetime)
+
+  return (
+    <li className="flex items-center px-4 py-2 space-x-4 group rounded-xl focus-within:bg-gray-100 hover:bg-gray-100">
+      <img
+        src={meeting.imageUrl}
+        alt=""
+        className="flex-none w-10 h-10 rounded-full"
+      />
+      <div className="flex-auto">
+        <p className="text-gray-900">{meeting.name}</p>
+        <p className="mt-0.5">
+          <time dateTime={meeting.startDatetime}>
+            {format(startDatetime, 'h:mm a')}
+          </time>{' '}
+          -{' '}
+          <time dateTime={meeting.endDatetime}>
+            {format(endDatetime, 'h:mm a')}
+          </time>
+        </p>
+      </div>
+      <Menu
+        as="div"
+        className="relative opacity-0 focus-within:opacity-100 group-hover:opacity-100"
+      >
+        <div>
+          <Menu.Button className="-m-2 flex items-center rounded-full p-1.5 text-gray-500 hover:text-gray-600">
+            <span className="sr-only">Open options</span>
+            <DotsVerticalIcon className="w-6 h-6" aria-hidden="true" />
+          </Menu.Button>
+        </div>
+        <Transition
+          as={Fragment}
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
+        >
+          <Menu.Items className="absolute right-0 z-10 mt-2 origin-top-right bg-white rounded-md shadow-lg w-36 ring-1 ring-black ring-opacity-5 focus:outline-none">
+            <div className="py-1">
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    Edit
+                  </a>
+                )}
+              </Menu.Item>
+              <Menu.Item>
+                {({ active }) => (
+                  <a
+                    href="#"
+                    className={classNames(
+                      active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                      'block px-4 py-2 text-sm'
+                    )}
+                  >
+                    Cancel
+                  </a>
+                )}
+              </Menu.Item>
+            </div>
+          </Menu.Items>
+        </Transition>
+      </Menu>
+    </li>
+  )
+}
+
+let colStartClasses = [
+  '',
+  'col-start-2',
+  'col-start-3',
+  'col-start-4',
+  'col-start-5',
+  'col-start-6',
+  'col-start-7',
+]
